b51c29bd93ae4cfbd688622133e1995044a27696
tangchaoyang <tangchaoyang@meituan.com>
2023-06-28 20:17:01 +0800

    完成用户界面的CRUD

--- src/main/java/com/nju/topology/controller/UserController.java
+++ src/main/java/com/nju/topology/controller/UserController.java
@@ -4,8 +4,14 @@ import com.nju.topology.common.Result;
 import com.nju.topology.entity.User;
 import com.nju.topology.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,35 +21,55 @@ import java.util.List;
  * @Date: 2023/6/23 23:01
  * @Description:
  */
-@RestController
-@RequestMapping("/user")
+@Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/list")
-    public Result<List<User>> getUserList() {
+    @GetMapping("/toUser")
+    public ModelAndView getUserList() {
         Result<List<User>> result = userService.getUserList();
-        return result;
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("user");
+        mv.addObject("userList", result.getData());
+        return mv;
     }
 
-    @PostMapping("/add")
-    public Result<String> addUser(@RequestBody User user) {
-        Result<String> result = userService.addUser(user);
-        return result;
+    @PostMapping("/addUser")
+    public ResponseEntity<String> addUser(
+                                          @RequestParam int studentId,
+                                          @RequestParam String name,
+                                          @RequestParam String password,
+                                          @RequestParam int type) {
+        Result<String> result = userService.addUser(new User(studentId, name, password, type));
+        if (result.getCode() == 1)
+            return ResponseEntity.ok(result.getData());
+        else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result.getData());
     }
 
-    @PostMapping("/update")
-    public Result<String> updateUser(@RequestBody User user) {
+    @PostMapping("/updateUser")
+    public ResponseEntity<String> updateUser(@RequestParam int id,
+                                     @RequestParam int studentId,
+                                     @RequestParam String name,
+                                     @RequestParam String password,
+                                     @RequestParam int type) {
+        User user = new User(studentId, name, password, type);
+        user.setId(id);
         Result<String> result = userService.updateUser(user);
-        return result;
+        if (result.getCode() == 1)
+            return ResponseEntity.ok(result.getData());
+        else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result.getData());
     }
 
-    @PostMapping("/delete")
-    public Result<String> deleteUser(@RequestParam int id) {
+    @PostMapping("/deleteUser")
+    public ResponseEntity<String> deleteUser(@RequestParam int id) {
         Result<String> result = userService.deleteUser(id);
-        return result;
+        if (result.getCode() == 1) {
+            return ResponseEntity.ok(result.getData());
+        } else {
+            String errorMessage = result.getData();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
     }
-
 }
--- src/main/java/com/nju/topology/entity/User.java
+++ src/main/java/com/nju/topology/entity/User.java
@@ -13,12 +13,61 @@ import lombok.Data;
 public class User {
     // 用户id
     private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
     // 学工号
     private int studentId;
     // 姓名
     private String name;
     // 密码
     private String password;
+
+    public User(int studentId, String name, String password, int type) {
+        this.studentId = studentId;
+        this.name = name;
+        this.password = password;
+        this.type = type;
+    }
+
     // 0-管理员；1-普通用户
     private int type;
 }
